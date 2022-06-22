c49bbe48a52bbb4a2839b0b295750f7bcde649d9
Devin Buhl <devin@buhl.casa>
2022-06-22 14:16:27 -0400

    chore: rolling commit
    Signed-off-by: Devin Buhl <devin@buhl.casa>

--- .github/scripts/fetch.sh
+++ .github/scripts/fetch.sh
@@ -12,7 +12,9 @@ find . -name metadata.json | while read -r metadata; do
         jq --arg v "$__latest_version" '.__current_version = $v' "${metadata}" | sponge "${metadata}"
         jq '.__build_status.__success = true' "${metadata}" | sponge "${metadata}"
         echo "${__app} | ${__stream} | ${__current_version} | ${__latest_version} | ${metadata}"
-        __container_versions+=("🌹 ${__app}-${__stream} updated from ${__current_version} to ${__latest_version}")
+        if [[ "${__latest_version}" !=  "${__current_version}" ]]; then
+            __container_versions+=("🌹 ${__app}-${__stream} updated from ${__current_version} to ${__latest_version}")
+        fi
     fi
 done
 
