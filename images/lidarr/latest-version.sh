beb7e80fb6a5c327564378b8a012ce5e832d4159
Devin Buhl <devin@buhl.casa>
2022-06-21 21:34:26 -0400

    chore: initial commit
    Signed-off-by: Devin Buhl <devin@buhl.casa>

---
+++ images/lidarr/latest-version.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+stream=$1
+version=$(curl -sX GET "https://lidarr.servarr.com/v1/update/${stream}/changes?os=linux&runtime=netcore" | jq --raw-output '.[0].version')
+version="${version#*v}"
+version="${version#*release-}"
+printf "%s" "${version}"
