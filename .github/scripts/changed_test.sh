f18fff3f91c7129a5396324f5f0f3051126abc3d
Devin Buhl <devin@buhl.casa>
2022-06-23 18:37:53 -0400

    chore: rolling commit
    Signed-off-by: Devin Buhl <devin@buhl.casa>

--- .github/scripts/changed_test.sh
+++ .github/scripts/changed_test.sh
@@ -3,8 +3,8 @@
 shopt -s lastpipe
 
 __changed_channels=()
-__app=$(echo "apps/lidarr/metadata.json5" | awk -F / '{print $2}')
-jq -c '.__channels | .[]' "apps/lidarr/metadata.json5" | while read -r __channels; do
+__app=$(echo "${{ matrix.apps }}" | awk -F / '{print $2}')
+jq -c '.__channels | .[]' "${{ matrix.apps }}" | while read -r __channels; do
     __name="$(jq --raw-output '.__name' <<< "${__channels}")"
     __version="$(jq --raw-output '.__version' <<< "${__channels}")"
     __fetched_version="$(jq --raw-output '.__fetched_version' <<< "${__channels}")"
@@ -13,9 +13,9 @@ jq -c '.__channels | .[]' "apps/lidarr/metadata.json5" | while read -r __channel
     fi
 done
 
-if (( ${#__changed_channels[@]} )); then
-    __channels="$(jo -a "${__changed_channels[@]}" | jq --raw-output)"
-fi
+# if (( ${#__changed_channels[@]} )); then
+#     __channels="$(jo -a "${__changed_channels[@]}" | jq --raw-output)"
+# fi
 
-echo "::set-output name=app::${__app}"
-echo "::set-output name=channels::${__channels}"
+echo "::set-output name=apps::$(jo -a "${__app}" | jq --raw-output)"
+echo "::set-output name=channels::$(jo -a "${__changed_channels[@]}" | jq --raw-output)"
