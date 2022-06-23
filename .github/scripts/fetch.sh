37cae0e509103b2f108f94147dc233fa752573e6
Devin Buhl <devin@buhl.casa>
2022-06-23 09:00:44 -0400

    chore: rolling commit
    Signed-off-by: Devin Buhl <devin@buhl.casa>

--- .github/scripts/fetch.sh
+++ .github/scripts/fetch.sh
@@ -8,12 +8,15 @@ find . -name metadata.json | while read -r metadata; do
     __current_version=$(jq --raw-output ".__current_version" "${metadata}")
     __stream=$(echo "${metadata}" | awk -F / '{print $4}')
     __latest_version="$(bash "$(dirname "$(dirname "${metadata}")")"/latest-version.sh "${__stream}")"
-    if [[ -n "${__latest_version}" || "${__latest_version}" != "null" ]]; then
-        jq --arg v "$__latest_version" '.__current_version = $v' "${metadata}" | sponge "${metadata}"
-        jq '.__build_status.__success = true' "${metadata}" | sponge "${metadata}"
-        echo "${__app} | ${__stream} | ${__current_version} | ${__latest_version} | ${metadata}"
-        if [[ "${__latest_version}" !=  "${__current_version}" ]]; then
-            __container_versions+=("⚡ Fetched new version for ${__app}-${__stream} (${__current_version} → ${__latest_version})")
+    __script="$(dirname "$(dirname "${metadata}")")/latest-version.sh)"
+    if test -f "${__script}"; then
+        if [[ -n "${__latest_version}" || "${__latest_version}" != "null" ]]; then
+            jq --arg v "$__latest_version" '.__current_version = $v' "${metadata}" | sponge "${metadata}"
+            jq '.__build_status.__success = true' "${metadata}" | sponge "${metadata}"
+            echo "${__app} | ${__stream} | ${__current_version} | ${__latest_version} | ${metadata}"
+            if [[ "${__latest_version}" !=  "${__current_version}" ]]; then
+                __container_versions+=("⚡ Fetched new version for ${__app}-${__stream} (${__current_version} → ${__latest_version})")
+            fi
         fi
     fi
 done
---
+++ apps/alpine/scripts/greeting.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+echo "
+You are entering the vicinity of an area adjacent to a location.
+The kind of place where there might be a monster, or some kind of weird mirror.
+These are just examples; it could also be something much better.
+"
