0cc14b12c99fa79b3d5dc1d56b84e797c255ee64
Devin Buhl <onedr0p@users.noreply.github.com>
2022-06-24 17:26:14 -0400

    Update fetch.sh

--- .github/scripts/fetch.sh
+++ .github/scripts/fetch.sh
@@ -7,9 +7,9 @@ find . -name metadata.json | while read -r metadata; do
     __app=$(echo "${metadata}" | awk -F / '{print $3}')
     __current_version=$(jq --raw-output ".__current_version" "${metadata}")
     __stream=$(echo "${metadata}" | awk -F / '{print $4}')
-    __latest_version="$(bash "$(dirname "$(dirname "${metadata}")")"/latest-version.sh "${__stream}")"
     __script="$(dirname "$(dirname "${metadata}")")/latest-version.sh)"
     if test -f "${__script}"; then
+         __latest_version="$(bash "${__script}" "${__stream}")"
         if [[ -n "${__latest_version}" || "${__latest_version}" != "null" ]]; then
             jq --arg v "$__latest_version" '.__current_version = $v' "${metadata}" | sponge "${metadata}"
             jq '.__build_status.__success = true' "${metadata}" | sponge "${metadata}"
