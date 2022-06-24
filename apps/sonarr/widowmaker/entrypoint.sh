b9635f4247a633b2e5105a691de588740fab533d
Devin Buhl <devin@buhl.casa>
2022-06-24 12:29:49 -0400

    chore: rolling commit
    Signed-off-by: Devin Buhl <devin@buhl.casa>

--- apps/sonarr/widowmaker/entrypoint.sh
+++ apps/sonarr/widowmaker/entrypoint.sh
@@ -22,7 +22,7 @@ if [[ -f /config/config.xml ]]; then
 fi
 
 # Update config.xml with environment variables
-/usr/local/bin/envsubst < /app/config.xml.tmpl > /config/config.xml
+envsubst < /app/config.xml.tmpl > /config/config.xml
 
 # Override configuation values from existing config.xml if there are no SONARR__ variables set
 [[ -z "${SONARR__LOG_LEVEL}" && -n "${current_log_level}" ]] && xmlstarlet edit --inplace --update //LogLevel -v "${current_log_level}" /config/config.xml
