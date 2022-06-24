a574f6f575b08dafca908de51b80de28e4c02755
Devin Buhl <devin@buhl.casa>
2022-06-23 23:19:35 -0400

    chore: rolling commit
    Signed-off-by: Devin Buhl <devin@buhl.casa>

--- apps/ubuntu/scripts/greeting.sh
+++ apps/ubuntu/scripts/greeting.sh
@@ -1,12 +1,7 @@
 #!/usr/bin/env bash
 
 echo "
-Welcome to a k8s@home container,
 You are entering the vicinity of an area adjacent to a location.
 The kind of place where there might be a monster, or some kind of weird mirror.
 These are just examples; it could also be something much better.
-* Repository: https://github.com/k8s-at-home/container-images
-* Docs: https://docs.k8s-at-home.com
-* Bugs or feature requests should be opened in an GH issue
-* Questions should be discussed in Discord or GH discussions
 "
