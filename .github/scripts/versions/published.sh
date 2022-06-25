eaaf24c5ff8d69a7c456cad2f35062cfcbe897a4
Devin Buhl <devin@buhl.casa>
2022-06-25 10:06:30 -0400

    chore: Add Sandbox
    Commit generated by whatthecommit.com
    
    Signed-off-by: Devin Buhl <devin@buhl.casa>

--- .github/scripts/versions/published.sh
+++ .github/scripts/versions/published.sh
@@ -8,18 +8,10 @@ if [[ -n "${APP}" && -n "${CHANNEL}" && "${STABLE}" != true ]]; then
     APP="${APP}-${CHANNEL}"
 fi
 
-# TODO: replace read-only packages token with workflow secret
-# tags=$( \
-#     curl -fsSL \
-#         -H "Accept: application/vnd.github.v3+json" \
-#         -H "Authorization: token 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" \
-#         "https://api.github.com/users/onedr0p/packages/container/${APP}/versions" \
-# )
-
 tags=$( \
     curl -fsSL \
         -H "Accept: application/vnd.github.v3+json" \
-        -H "Authorization: token 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" \
+        -H "Authorization: token ${TOKEN}" \
         "https://api.github.com/users/onedr0p/packages/container/${APP}/versions" \
 )
 
