95543a00e33daef192744e6a00479f9e00c322b9
Devin Buhl <devin@buhl.casa>
2022-06-25 09:33:30 -0400

    chore: Fixed Bug
    Commit generated by whatthecommit.com
    
    Signed-off-by: Devin Buhl <devin@buhl.casa>

--- .github/scripts/versions/published.sh
+++ .github/scripts/versions/published.sh
@@ -9,6 +9,13 @@ if [[ -n "${APP}" && -n "${CHANNEL}" && "${STABLE}" != true ]]; then
 fi
 
 # TODO: replace read-only packages token with workflow secret
+# tags=$( \
+#     curl -fsSL \
+#         -H "Accept: application/vnd.github.v3+json" \
+#         -H "Authorization: token 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" \
+#         "https://api.github.com/users/onedr0p/packages/container/${APP}/versions" \
+# )
+
 tags=$( \
     curl -fsSL \
         -H "Accept: application/vnd.github.v3+json" \
