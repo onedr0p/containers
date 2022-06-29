49323411834ff69510ba79d92731b618b846265e
Devin Buhl <devin@buhl.casa>
2022-06-29 08:32:25 -0400

    fix: flood args
    Signed-off-by: Devin Buhl <devin@buhl.casa>

--- apps/flood/entrypoint.sh
+++ apps/flood/entrypoint.sh
@@ -7,8 +7,10 @@ test -f "/scripts/vpn.sh" && source "/scripts/vpn.sh"
 #shellcheck disable=SC2086
 exec \
     /app/flood \
-        --rundir "/config" \
+        --rundir /config \
+        --allowedpath /config \
+        --allowedpath /dev/shm \
         --host 0.0.0.0 \
         --port 3001 \
-        --rtsocket "/dev/shm/rtorrent.sock" \
+        --rtsocket /dev/shm/rtorrent.sock \
         ${EXTRA_ARGS}
