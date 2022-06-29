7e04096dff50c7a8e4355a4fa622c7860e85df42
Devin Buhl <devin@buhl.casa>
2022-06-29 09:08:36 -0400

    fix: add default rtorrent.rc
    Signed-off-by: Devin Buhl <devin@buhl.casa>

--- apps/rtorrent/entrypoint.sh
+++ apps/rtorrent/entrypoint.sh
@@ -4,6 +4,10 @@
 test -f "/scripts/umask.sh" && source "/scripts/umask.sh"
 test -f "/scripts/vpn.sh" && source "/scripts/vpn.sh"
 
+if [[ ! -f "/config/rtorrent.rc" ]]; then
+    cp /app/rtorrent.rc /config/rtorrent.rc
+fi
+
 #shellcheck disable=SC2086
 exec \
     /app/rtorrent \
