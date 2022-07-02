980fca1ad5900bfce9dcedae64cfa7ad729fa62c
Devin Buhl <devin@buhl.casa>
2022-07-02 15:11:54 -0400

    fix: let sabnzbd api and nzb key be configured 12factorish
    Signed-off-by: Devin Buhl <devin@buhl.casa>

--- apps/sabnzbd/entrypoint.sh
+++ apps/sabnzbd/entrypoint.sh
@@ -9,28 +9,16 @@ if [[ ! -f "/config/sabnzbd.ini" ]]; then
     mkdir -p /config/sabnzbd
     cp /app/sabnzbd.ini /config/sabnzbd.ini
 
-    if [[ -n "${SABNZBD__API_KEY}" ]]; then
-        api_key="${SABNZBD__API_KEY}"
-    else
-        printf "Creating api key ..."
-        api_key=$(cat /dev/urandom | tr -dc 'a-z0-9' | fold -w 32 | head -n 1)
-    fi
-
-    if [[ -n "${SABNZBD__NZB_KEY}" ]]; then
-        nzb_key="${SABNZBD__NZB_KEY}"
-    else
-        printf "Creating nzb key ..."
-        nzb_key=$(cat /dev/urandom | tr -dc 'a-z0-9' | fold -w 32 | head -n 1)
-    fi
-
+    printf "Creating api keys ..."
+    api_key=$(cat /dev/urandom | tr -dc 'a-z0-9' | fold -w 32 | head -n 1)
+    nzb_key=$(cat /dev/urandom | tr -dc 'a-z0-9' | fold -w 32 | head -n 1)
     sed -i -e "s/^api_key *=.*$/api_key = ${api_key}/g" /config/sabnzbd.ini
     sed -i -e "s/^nzb_key *=.*$/nzb_key = ${nzb_key}/g" /config/sabnzbd.ini
 fi
 
-if [[ -n ${SABNZBD__HOST_WHITELIST_ENTRIES} ]]; then
-    printf "Updating host_whitelist setting ... "
-    sed -i -e "s/^host_whitelist *=.*$/host_whitelist = ${HOSTNAME:-sabnzbd}, ${SABNZBD__HOST_WHITELIST_ENTRIES}/g" /config/sabnzbd.ini
-fi
+[[ -n "${SABNZBD__API_KEY}" ]] && sed -i -e "s/^api_key *=.*$/api_key = ${SABNZBD__API_KEY}/g" /config/sabnzbd.ini
+[[ -n "${SABNZBD__NZB_KEY}" ]] && sed -i -e "s/^nzb_key *=.*$/nzb_key = ${SABNZBD__NZB_KEY}/g" /config/sabnzbd.ini
+[[ -n "${SABNZBD__HOST_WHITELIST_ENTRIES}" ]] && sed -i -e "s/^host_whitelist *=.*$/host_whitelist = ${HOSTNAME:-sabnzbd}, ${SABNZBD__HOST_WHITELIST_ENTRIES}/g" /config/sabnzbd.ini
 
 #shellcheck disable=SC2086
 exec \
