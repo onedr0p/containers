b4963a1fe529a2b873b116d0664df3bb2f03cc0e
Devin Buhl <devin@buhl.casa>
2022-07-04 11:04:03 -0400

    fix: pass all args to entrypoint.sh to apps exec cmd
    Signed-off-by: Devin Buhl <devin@buhl.casa>

--- apps/jbops/entrypoint.sh
+++ apps/jbops/entrypoint.sh
@@ -4,8 +4,7 @@
 test -f "/scripts/umask.sh" && source "/scripts/umask.sh"
 test -f "/scripts/vpn.sh" && source "/scripts/vpn.sh"
 
-#shellcheck disable=SC2086
 exec \
     /usr/bin/python3 \
-        /app/${JBOPS__SCRIPT_PATH} \
-        ${EXTRA_ARGS}
+        "/app/${JBOPS__SCRIPT_PATH}" \
+        "$@"
