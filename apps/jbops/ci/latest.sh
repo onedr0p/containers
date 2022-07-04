239792784970fe5d65341093b2ad9ea8b926f1ae
Devin Buhl <devin@buhl.casa>
2022-07-04 08:53:57 -0400

    feat: add jbops container
    Signed-off-by: Devin Buhl <devin@buhl.casa>

--- apps/jbops/ci/latest.sh
+++ apps/jbops/ci/latest.sh
@@ -4,4 +4,4 @@ pushd /tmp/jbops > /dev/null || exit
 number_of_commits=$(git rev-list --count --first-parent HEAD)
 popd > /dev/null || exit
 rm -rf /tmp/jbops
-printf "%d.%d.%d" "1" "0" "${number_of_commits}"
+printf "1.0.%d" "${number_of_commits}"
