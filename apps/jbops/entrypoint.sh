60214b8886e1829ce6c368368fb32842e48bed17
Devin Buhl <devin@buhl.casa>
2022-07-04 08:46:00 -0400

    feat: add jbops container
    Signed-off-by: Devin Buhl <devin@buhl.casa>

--- apps/jbops/entrypoint.sh
+++ apps/jbops/entrypoint.sh
@@ -4,10 +4,6 @@
 test -f "/scripts/umask.sh" && source "/scripts/umask.sh"
 test -f "/scripts/vpn.sh" && source "/scripts/vpn.sh"
 
-if [[ ! -f /config/config.ini ]]; then
-    cp /app/config.ini /config/config.ini
-fi
-
 #shellcheck disable=SC2086
 exec \
     /usr/bin/python3 \
