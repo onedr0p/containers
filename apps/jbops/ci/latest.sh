8963c3c0b226b13e4aa37ef0b115a59b101ce2d1
Devin Buhl <devin@buhl.casa>
2022-07-04 09:00:58 -0400

    feat: add jbops container
    Signed-off-by: Devin Buhl <devin@buhl.casa>

--- apps/jbops/ci/latest.sh
+++ apps/jbops/ci/latest.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 git clone --quiet https://github.com/blacktwin/JBOPS.git /tmp/jbops
 pushd /tmp/jbops > /dev/null || exit
-number_of_commits=$(git rev-list --count --first-parent HEAD)
+version=$(git rev-list --count --first-parent HEAD)
 popd > /dev/null || exit
 rm -rf /tmp/jbops
-printf "1.0.%d" "${number_of_commits}"
+printf "1.0.%d" "${version}"
