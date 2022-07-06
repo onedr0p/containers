86eb5e87e0291c234bae4c65765521eb084a91f9
Devin Buhl <onedr0p@users.noreply.github.com>
2022-07-06 16:18:10 -0400

    feat: add home assistant (#38)

---
+++ apps/home-assistant/entrypoint.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+#shellcheck disable=SC1091
+test -f "/scripts/umask.sh" && source "/scripts/umask.sh"
+test -f "/scripts/vpn.sh" && source "/scripts/vpn.sh"
+
+if [[ "${HOME_ASSISTANT__HACS_INSTALL}" == "true" ]]; then
+    wget -O - https://get.hacs.xyz | bash -
+fi
+
+exec \
+    /usr/bin/hass \
+        --config /config \
+        "$@"
