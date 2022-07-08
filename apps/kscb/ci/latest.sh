9d7cb6412f8c8803b0ae5d8538ba5ac1a71bcd47
Bᴇʀɴᴅ Sᴄʜᴏʀɢᴇʀs <me@bjw-s.dev>
2022-07-08 16:20:18 +0200

    feat(kscb)!: Remove

--- apps/kscb/ci/latest.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-version=$(curl -sX GET "https://api.github.com/repos/prodrigestivill/go-cron/releases/latest" | jq --raw-output '.tag_name')
-version="${version%%_*}"
-version="${version%%-*}"
-printf "%s" "${version}"
