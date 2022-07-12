ba9714507e89a573b88ef7dc6bf679b49a73ea8a
Devin Buhl <devin@buhl.casa>
2022-07-12 12:22:02 -0400

    fix: arm64 go brr
    Signed-off-by: Devin Buhl <devin@buhl.casa>

--- apps/tvheadend/entrypoint.sh
+++ apps/tvheadend/entrypoint.sh
@@ -4,6 +4,15 @@
 test -f "/scripts/umask.sh" && source "/scripts/umask.sh"
 test -f "/scripts/vpn.sh" && source "/scripts/vpn.sh"
 
+mkdir -p /config/comskip
+
+[[ ! -f /config/dvr/config ]] && \
+	(mkdir -p /config/dvr/config && cp /app/defaults/7a5edfbe189851e5b1d1df19c93962f0 /config/dvr/config/7a5edfbe189851e5b1d1df19c93962f0)
+[[ ! -f /config/comskip/comskip.ini ]] && \
+    (cp /app/defaults/comskip.ini.org /config/comskip/comskip.ini)
+[[ ! -f /config/config ]] && \
+    (cp /app/defaults/config /config/config)
+
 #shellcheck disable=SC2086
 exec \
     /usr/bin/tvheadend -C \
