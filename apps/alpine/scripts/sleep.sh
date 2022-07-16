67b14d836f0cc9af2d67cb3da67dc9370736bbee
Devin Buhl <devin@buhl.casa>
2022-07-16 10:08:24 -0400

    feat: Update sleep script in base images
    Signed-off-by: Devin Buhl <devin@buhl.casa>

--- apps/alpine/scripts/sleep.sh
+++ apps/alpine/scripts/sleep.sh
@@ -2,16 +2,27 @@
 
 min_seconds="${1:-1}"
 max_seconds="${2:-3600}"
+seconds="$(shuf -i "${min_seconds}"-"${max_seconds}" -n 1)"
 
-printf "\e[1;32m%-6s\e[m\n" "INFO: Minimum Seconds set to ${min_seconds}"
-printf "\e[1;32m%-6s\e[m\n" "INFO: Maximum Seconds set to ${max_seconds}"
+function logz {
+    msg="${1}"
+    level="${2:-info}"
+    printf "\e[1;32m%-6s\e[m\n" "timestamp=\"$(date --rfc-3339=seconds)\" level=\"${level}\" msg=\"${msg}\""
+}
 
-if [[ -z "${seconds}" ]]; then
-    seconds="$(shuf -i "${min_seconds}"-"${max_seconds}" -n 1)"
-fi
+function datez {
+    secs="${1}"
+    printf "%dh%dm%ds" $((secs/3600)) $((secs%3600/60)) $((secs%60))
+}
 
-printf "\e[1;32m%-6s\e[m\n" "INFO: Sleeping for ${seconds}s ..."
+printf "\e[1;32m%-6s\e[m\n" "$(logz "min seconds set to ${min_seconds}" "debug")"
+printf "\e[1;32m%-6s\e[m\n" "$(logz "max seconds set to ${max_seconds}" "debug")"
 
-yes | pv -SL1 -F "Resuming in %e" -s "${seconds}" > /dev/null
+printf "\e[1;32m%-6s\e[m\n" "$(logz "sleeping for $(datez "${seconds}")" "info")"
 
-printf "\e[1;32m%-6s\e[m\n" "INFO: Done"
+for ((i=seconds;i>0;i--)); do 
+    printf "\e[1;32m%-6s\e[m\n" "$(logz "sleeping for $(datez "${i}")" "info")"
+    sleep 1
+done
+
+printf "\e[1;32m%-6s\e[m\n" "$(logz "done" "debug")"
