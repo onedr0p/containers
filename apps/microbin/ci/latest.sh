2dbae102ff98e0f0e30e025e67ce818c7dac8365
Devin Buhl <onedr0p@users.noreply.github.com>
2022-08-10 16:55:30 -0400

    feat: delete microbin

--- apps/microbin/ci/latest.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-version=$(curl -sX GET "https://api.github.com/repos/szabodanika/microbin/tags" | jq --raw-output '.[0] | .name')
-version="${version#*v}"
-version="${version#*release-}"
-printf "%s" "${version}"
