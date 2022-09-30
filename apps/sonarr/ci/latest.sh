91cbbdcac7ae3b507c3f8d007a1144cac6aa1f40
Devin Buhl <devin@buhl.casa>
2022-09-30 10:13:45 -0400

    feat(sonarr)!: Switch develop to v4 and deprecate widowmaker
    Signed-off-by: Devin Buhl <devin@buhl.casa>

--- apps/sonarr/ci/latest.sh
+++ apps/sonarr/ci/latest.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 channel=$1
 
-if [[ "${channel}" == "widowmaker" ]]; then
-    version=$(curl -sX GET "https://sonarr.servarr.com/v1/update/${channel}/changes?os=linux&runtime=netcore" | jq --raw-output '.[0].version')
+if [[ "${channel}" == "develop" ]]; then
+    version=$(curl -sX GET "https://services.sonarr.tv/v1/download/${channel}?version=4.0" | jq --raw-output '.version')
 else
     version=$(curl -sX GET "https://services.sonarr.tv/v1/download/${channel}?version=3" | jq --raw-output '.version')
 fi
