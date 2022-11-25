55f6d83ccd4902d003591b83127f2900412abd37
Devin Buhl <devin@buhl.casa>
2022-11-25 13:35:50 -0500

    feat: Bump ubuntu to jammy and use upx on arm64 binaries
    Signed-off-by: Devin Buhl <devin@buhl.casa>

--- apps/qbittorrent/ci/latest.sh
+++ apps/qbittorrent/ci/latest.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# version=$(curl -sX GET "https://pkgs.alpinelinux.org/packages?name=qbittorrent-nox&branch=v3.16&arch" | grep -oP '(?<=<td class="version">)[^<]*')
+# version=$(curl -sX GET "https://pkgs.alpinelinux.org/packages?name=qbittorrent-nox&branch=v3.17&arch" | grep -oP '(?<=<td class="version">)[^<]*')
 version=$(curl -sX GET "https://repology.org/api/v1/projects/?search=qbittorrent&inrepo=alpine_edge" | jq -r '.qbittorrent | .[] | select((.repo == "alpine_edge" and .binname == "qbittorrent-nox")) | .version')
 version="${version%%_*}"
 version="${version%%-*}"
