5805d533f139caeb202f1f0bef267ff0f7d48a24
Anthony Rabbito <hello@anthonyrabbito.com>
2022-11-25 15:37:07 -0500

    feat(plex): add support for AMDGPU (#96)

--- apps/plex/ci/latest.sh
+++ apps/plex/ci/latest.sh
@@ -14,6 +14,10 @@ if [[ "${channel}" == "stable" ]]; then
     version=$(curl -sX GET 'https://plex.tv/api/downloads/5.json' | jq -r '.computer.Linux.version')
 fi
 
+if [[ "${channel}" == "amd" ]]; then
+    version=$(curl -sX GET 'https://plex.tv/api/downloads/5.json' | jq -r '.computer.Linux.version')
+fi
+
 version="${version#*v}"
 version="${version#*release-}"
 printf "%s" "${version}"
