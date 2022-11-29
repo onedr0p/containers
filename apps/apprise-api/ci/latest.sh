0dbff9942e02f60651e4a8caf1dd454fe5295bb8
Devin Buhl <onedr0p@users.noreply.github.com>
2022-11-28 21:33:30 -0500

    feat!: Deprecate apprise-api

--- apps/apprise-api/ci/latest.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-version=$(curl -sX GET "https://api.github.com/repos/caronc/apprise-api/releases/latest" | jq --raw-output '.tag_name')
-version="${version#*v}"
-version="${version#*release-}"
-printf "%s" "${version}"
