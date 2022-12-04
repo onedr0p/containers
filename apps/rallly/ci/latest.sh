580cdb8d476c04f7efb01cd1cf164049e7d4a5bf
Devin Buhl <onedr0p@users.noreply.github.com>
2022-12-04 09:31:46 -0500

    feat!: Deprecate rallly

--- apps/rallly/ci/latest.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-git clone --quiet https://github.com/lukevella/rallly.git /tmp/rallly
-pushd /tmp/rallly > /dev/null || exit
-version=$(git rev-list --count --first-parent HEAD)
-popd > /dev/null || exit
-rm -rf /tmp/rallly
-printf "1.0.%d" "${version}"
