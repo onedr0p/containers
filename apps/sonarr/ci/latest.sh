660368a9ec6f2ba778d0097041ac482d1cc6eac9
Devin Buhl <devin@buhl.casa>
2022-12-24 13:52:04 -0500

    fix: redirect stderr to dev null on upstream version checks
    Signed-off-by: Devin Buhl <devin@buhl.casa>

--- apps/sonarr/ci/latest.sh
+++ apps/sonarr/ci/latest.sh
@@ -2,9 +2,9 @@
 channel=$1
 
 if [[ "${channel}" == "develop" ]]; then
-    version=$(curl -sX GET "https://services.sonarr.tv/v1/download/${channel}?version=4.0" | jq --raw-output '.version')
+    version=$(curl -sX GET "https://services.sonarr.tv/v1/download/${channel}?version=4.0" | jq --raw-output '.version' 2>/dev/null)
 else
-    version=$(curl -sX GET "https://services.sonarr.tv/v1/download/${channel}?version=3" | jq --raw-output '.version')
+    version=$(curl -sX GET "https://services.sonarr.tv/v1/download/${channel}?version=3" | jq --raw-output '.version' 2>/dev/null)
 fi
 
 version="${version#*v}"
