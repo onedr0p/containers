e29dc54e3c6a4cddb8fdc836886e8a9dbae7a771
Devin Buhl <devin@buhl.casa>
2023-05-10 09:16:09 -0400

    fix: update postgres-initdb to pull from 3.18
    Signed-off-by: Devin Buhl <devin@buhl.casa>

--- apps/postgres-initdb/ci/latest.sh
+++ apps/postgres-initdb/ci/latest.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-version=$(curl -sX GET "https://pkgs.alpinelinux.org/packages?name=postgresql14-client&branch=v3.17&arch" | grep -oP '(?<=<td class="version">)[^<]*' 2>/dev/null)
+version=$(curl -sX GET "https://pkgs.alpinelinux.org/packages?name=postgresql14-client&branch=v3.18&arch" | grep -oP '(?<=<td class="version">)[^<]*' 2>/dev/null)
 version="${version%%_*}"
 version="${version%%-*}"
 printf "%s" "${version}"
