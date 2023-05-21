c0486037df4eee40b1b05206a287e40f2c4efdcf
Devin Buhl <devin@buhl.casa>
2023-05-21 18:03:53 -0400

    feat: add github-actions-runner container
    Signed-off-by: Devin Buhl <devin@buhl.casa>

---
+++ apps/github-actions-runner/ci/latest.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+version=$(curl -sX GET "https://api.github.com/repos/actions/runner/releases/latest" | jq --raw-output '.tag_name')
+version="${version#*v}"
+version="${version#*release-}"
+printf "%s" "${version}"
