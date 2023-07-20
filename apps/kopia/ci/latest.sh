476c7287c2dfcc438fd0d85f3fe3b48f96c2c679
Devin Buhl <devin@buhl.casa>
2023-07-20 07:31:44 -0400

    fix: deprecate rclone and kopia containers
    Signed-off-by: Devin Buhl <devin@buhl.casa>

--- apps/kopia/ci/latest.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-version=$(curl -sX GET "https://api.github.com/repos/kopia/kopia/releases/latest" | jq --raw-output '.tag_name' 2>/dev/null)
-version="${version#*v}"
-version="${version#*release-}"
-printf "%s" "${version}"
