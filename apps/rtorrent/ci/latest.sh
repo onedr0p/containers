a2351212cf2edfc6734caec8d0fadfc27bc45044
Devin Buhl <devin@buhl.casa>
2023-11-03 17:59:37 -0400

    fix: deprecate flood and rtorrent
    Signed-off-by: Devin Buhl <devin@buhl.casa>

--- apps/rtorrent/ci/latest.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-version=$(curl -sX GET "https://api.github.com/repos/jesec/rtorrent/releases" | jq --raw-output '.[0].tag_name' 2>/dev/null)
-version="${version#*v}"
-version="${version#*release-}"
-printf "%s" "${version}"
