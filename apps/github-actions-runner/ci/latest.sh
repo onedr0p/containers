809d701708d2e4e67614c2945e6b37c8244e08e4
Devin Buhl <onedr0p@users.noreply.github.com>
2023-11-04 09:05:40 -0400

    Deprecate github-actions-runner

--- apps/github-actions-runner/ci/latest.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-version=$(curl -sX GET "https://api.github.com/repos/actions/runner/releases/latest" | jq --raw-output '.tag_name')
-version="${version#*v}"
-version="${version#*release-}"
-printf "%s" "${version}"
