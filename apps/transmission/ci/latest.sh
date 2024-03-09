3609b724c587b357072c3d70ee9bd3d0d6ff749c
Devin Buhl <onedr0p@users.noreply.github.com>
2024-03-09 06:42:24 -0500

    feat: deprecate transmission (#971)
    I'm only looking to maintain qb for here in out.

--- apps/transmission/ci/latest.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-version=$(curl -sX GET "https://repology.org/api/v1/projects/?search=transmission&inrepo=alpine_3_19" | jq -r '.transmission | .[] | select((.repo == "alpine_3_19" and .binname == "transmission-daemon")) | .origversion')
-version="${version%%_*}"
-version="${version%%-*}"
-printf "%s" "${version}"
