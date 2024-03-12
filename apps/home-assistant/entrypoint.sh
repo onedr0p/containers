5bdb02eb117d0e507ab95e458ad9da9f6b2e8092
Devin Buhl <devin@buhl.casa>
2024-03-12 10:33:46 -0400

    feat(hass): update entrypoint to log to /config/logs
    Signed-off-by: Devin Buhl <devin@buhl.casa>

--- apps/home-assistant/entrypoint.sh
+++ apps/home-assistant/entrypoint.sh
@@ -3,6 +3,10 @@
 #shellcheck disable=SC1091
 test -f "/scripts/umask.sh" && source "/scripts/umask.sh"
 
+# Make sure the config and log directory exist
+mkdir -p /config/logs
+
+# Install HACS if requested
 if [[ "${HOME_ASSISTANT__HACS_INSTALL}" == "true" ]]; then
     wget -O - https://get.hacs.xyz | bash -
 fi
@@ -10,4 +14,5 @@ fi
 exec \
     /usr/local/bin/hass \
         --config /config \
+        --log-file /config/logs/home-assistant.log \
         "$@"
