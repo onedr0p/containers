2704e50e8a4cbc59a4d87be55d26d39f47a29e8d
Devin Buhl <onedr0p@users.noreply.github.com>
2024-03-12 10:42:10 -0400

    feat(hass): build with upstream python 3.12-alpine (#977)
    * feat(hass): build with upstream python 3.12-alpine
    
    Signed-off-by: Devin Buhl <devin@buhl.casa>
    
    * feat(hass): update entrypoint script
    
    Signed-off-by: Devin Buhl <devin@buhl.casa>
    
    * feat(hass): update entrypoint to log to /config/logs
    
    Signed-off-by: Devin Buhl <devin@buhl.casa>
    
    ---------
    
    Signed-off-by: Devin Buhl <devin@buhl.casa>

--- apps/home-assistant/entrypoint.sh
+++ apps/home-assistant/entrypoint.sh
@@ -3,11 +3,16 @@
 #shellcheck disable=SC1091
 test -f "/scripts/umask.sh" && source "/scripts/umask.sh"
 
+# Make sure the config and log directory exist
+mkdir -p /config/logs
+
+# Install HACS if requested
 if [[ "${HOME_ASSISTANT__HACS_INSTALL}" == "true" ]]; then
     wget -O - https://get.hacs.xyz | bash -
 fi
 
 exec \
-    /usr/bin/hass \
+    /usr/local/bin/hass \
         --config /config \
+        --log-file /config/logs/home-assistant.log \
         "$@"
