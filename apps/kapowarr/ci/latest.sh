47ab9f433707dc3c54b7b8b9a4511efd59ab793f
Tom Parker <tom.parker00+2@gmail.com>
2024-05-13 16:10:46 -0400

    Update latest.sh
    There are releases now

--- apps/kapowarr/ci/latest.sh
+++ apps/kapowarr/ci/latest.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 #version=$(curl -H "Accept: application/vnd.github+json" https://api.github.com/repos/Casvt/kapowarr/releases | jq -r 'sort_by(.published_at) | reverse | .[].name' | head -n 1)
 # We don't have regular releases yet
-version=$(curl -s https://api.github.com/repos/Casvt/kapowarr/commits | jq -r 'sort_by(.published_at) | .[].sha' | head -n 1)
+version=$(curl -s https://api.github.com/repos/Casvt/kapowarr/releases/latest | jq --raw-output '.tag_name')
+version="${version#*v}"
+version="${version#*release-}"
 printf "%s" "${version}"
