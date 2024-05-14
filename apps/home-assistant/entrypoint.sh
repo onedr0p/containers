66ffb4ff33e586e41f4f2505d7e6d933f4e0330e
Devin Buhl <onedr0p@users.noreply.github.com>
2024-05-14 13:44:42 -0400

    Discard changes to apps/home-assistant/entrypoint.sh

--- apps/home-assistant/entrypoint.sh
+++ apps/home-assistant/entrypoint.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+mkdir -p /config/logs
+
 if [[ "${HOME_ASSISTANT__HACS_INSTALL}" == "true" ]]; then
     curl -sfSL https://hacs.xyz/install | bash -
 fi
@@ -7,4 +9,5 @@ fi
 exec \
     /usr/local/bin/hass \
         --config /config \
+        --log-file /config/logs/home-assistant.log \
         "$@"
