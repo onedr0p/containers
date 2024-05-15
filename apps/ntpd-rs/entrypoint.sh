c211ab2b114dbc0eafe3651e4569e257b5612edf
Devin Buhl <devin@buhl.casa>
2024-05-15 09:21:55 -0400

    fix: update ntpd-rs
    Signed-off-by: Devin Buhl <devin@buhl.casa>

--- apps/ntpd-rs/entrypoint.sh
+++ apps/ntpd-rs/entrypoint.sh
@@ -2,10 +2,11 @@
 
 if [[ ! -f "/config/ntp.toml" ]]; then
     printf "Copying over default configuration ...\n"
-    cp /etc/ntp.toml /config/ntp.toml
+    cp /etc/ntpd-rs/ntp.toml /config/ntp.toml
 fi
 
 #shellcheck disable=SC2086
 exec \
     /usr/local/bin/ntp-daemon \
-        --config /config/ntp.toml
+        --config /config/ntp.toml \
+        --log-level debug
