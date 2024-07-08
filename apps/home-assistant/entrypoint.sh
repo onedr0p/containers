c686122c8e86280dbe193056bbf3b9ad759a95e9
Jeff Wallace <jeff@tjwallace.ca>
2024-07-08 11:37:50 -0700

    fix(home-assistant): update HACS install url
    See https://hacs.xyz/docs/setup/download/

--- apps/home-assistant/entrypoint.sh
+++ apps/home-assistant/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if [[ "${HOME_ASSISTANT__HACS_INSTALL}" == "true" ]]; then
-    curl -sfSL https://hacs.xyz/install | bash -
+    curl -sfSL https://get.hacs.xyz | bash -
 fi
 
 exec \
