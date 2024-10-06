c657539c8ce3e4b489f87cce864ccf29f16c390b
Devin Buhl <devin@buhl.casa>
2024-10-05 20:47:08 -0400

    fix: updates
    Signed-off-by: Devin Buhl <devin@buhl.casa>

--- apps/qbittorrent/entrypoint.sh
+++ apps/qbittorrent/entrypoint.sh
@@ -4,19 +4,22 @@
 CONFIG_FILE="/config/qBittorrent/qBittorrent.conf"
 LOG_FILE="/config/qBittorrent/logs/qbittorrent.log"
 
-export QBT_WEBUI_PORT=${QBITTORRENT__PORT:-$QBT_WEBUI_PORT} # QBITTORRENT__PORT is deprecated
-export QBT_TORRENTING_PORT=${QBITTORRENT__BT_PORT:-$QBT_TORRENTING_PORT} # QBITTORRENT__BT_PORT is deprecated
+# Set environment variables (QBITTORRENT__PORT and QBITTORRENT__BT_PORT are deprecated)
+export QBT_WEBUI_PORT="${QBITTORRENT__PORT:-$QBT_WEBUI_PORT}"
+export QBT_TORRENTING_PORT="${QBITTORRENT__BT_PORT:-$QBT_TORRENTING_PORT}"
 
+# Ensure the config file exists, copy default if missing
 if [[ ! -f "${CONFIG_FILE}" ]]; then
-    printf "Copying over default configuration ...\n"
-    mkdir -p "$(dirname ${CONFIG_FILE})"
+    echo "Copying over default configuration ..."
+    mkdir -p "${CONFIG_FILE%/*}"
     cp /app/qBittorrent.conf "${CONFIG_FILE}"
 fi
 
 # Set up log file to redirect to stdout
 if [[ ! -f "${LOG_FILE}" ]]; then
-    mkdir -p "$(dirname ${LOG_FILE})"
+    mkdir -p "${LOG_FILE%/*}"
     ln -sf /proc/self/fd/1 "${LOG_FILE}"
 fi
 
+# Execute qBittorrent
 exec /app/qbittorrent-nox "$@"
