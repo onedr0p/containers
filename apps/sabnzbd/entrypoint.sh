b7441d93cf9dc0bf97f2667991ebefc5f7aed227
Devin Buhl <devin@buhl.casa>
2024-10-06 16:53:19 -0400

    fix(sabnzbd): uninstall uv when done
    Signed-off-by: Devin Buhl <devin@buhl.casa>

--- apps/sabnzbd/entrypoint.sh
+++ apps/sabnzbd/entrypoint.sh
@@ -1,27 +1,40 @@
 #!/usr/bin/env bash
 #shellcheck disable=SC2086
 
+set -euo pipefail
+
+# Function to update the sabnzbd.ini file
+update_config() {
+    local key=$1
+    local value=$2
+    sed -i -e "s/^${key} *=.*$/${key} = ${value}/g" /config/sabnzbd.ini
+}
+
 if [[ ! -f "/config/sabnzbd.ini" ]]; then
     printf "Copying over default configuration ...\n"
     mkdir -p /config/sabnzbd
     cp /app/sabnzbd.ini /config/sabnzbd.ini
 
     printf "Creating api keys ...\n"
-    api_key=$(cat /dev/urandom | tr -dc 'a-z0-9' | fold -w 32 | head -n 1)
-    nzb_key=$(cat /dev/urandom | tr -dc 'a-z0-9' | fold -w 32 | head -n 1)
-    sed -i -e "s/^api_key *=.*$/api_key = ${api_key}/g" /config/sabnzbd.ini
-    sed -i -e "s/^nzb_key *=.*$/nzb_key = ${nzb_key}/g" /config/sabnzbd.ini
+    api_key=$(tr -dc 'a-z0-9' < /dev/urandom | fold -w 32 | head -n 1)
+    nzb_key=$(tr -dc 'a-z0-9' < /dev/urandom | fold -w 32 | head -n 1)
+
+    update_config "api_key" "${api_key}"
+    update_config "nzb_key" "${nzb_key}"
 fi
 
-[[ -n "${SABNZBD__API_KEY}" ]] && sed -i -e "s/^api_key *=.*$/api_key = ${SABNZBD__API_KEY}/g" /config/sabnzbd.ini
-[[ -n "${SABNZBD__NZB_KEY}" ]] && sed -i -e "s/^nzb_key *=.*$/nzb_key = ${SABNZBD__NZB_KEY}/g" /config/sabnzbd.ini
-[[ -n "${SABNZBD__HOST_WHITELIST_ENTRIES}" ]] && sed -i -e "s/^host_whitelist *=.*$/host_whitelist = ${HOSTNAME:-sabnzbd}, ${SABNZBD__HOST_WHITELIST_ENTRIES}/g" /config/sabnzbd.ini
+[[ -n "${SABNZBD__API_KEY:-}" ]] && update_config "api_key" "${SABNZBD__API_KEY}"
+[[ -n "${SABNZBD__NZB_KEY:-}" ]] && update_config "nzb_key" "${SABNZBD__NZB_KEY}"
+
+if [[ -n "${SABNZBD__HOST_WHITELIST_ENTRIES:-}" ]]; then
+    update_config "host_whitelist" "${HOSTNAME:-sabnzbd}, ${SABNZBD__HOST_WHITELIST_ENTRIES}"
+fi
 
 exec \
     /usr/local/bin/python \
         /app/SABnzbd.py \
         --browser 0 \
-        --server ${SABNZBD__ADDRESS}:${SABNZBD__PORT} \
+        --server "${SABNZBD__ADDRESS}:${SABNZBD__PORT}" \
         --config-file /config/sabnzbd.ini \
         --disable-file-log \
         --console \
