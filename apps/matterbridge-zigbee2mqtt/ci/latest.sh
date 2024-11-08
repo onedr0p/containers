3dbd6ad48019cf7025d144d8c32c71af2d70ff61
Judah Rand <17158624+judahrand@users.noreply.github.com>
2024-11-08 15:06:47 +0000

    Delete `matterbridge` containers

--- apps/matterbridge-zigbee2mqtt/ci/latest.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-channel=$1
-version=$(curl -sX GET "https://registry.npmjs.org/matterbridge-zigbee2mqtt" | jq --raw-output '."dist-tags".latest' 2>/dev/null)
-version="${version#*v}"
-version="${version#*release-}"
-printf "%s" "${version}"
