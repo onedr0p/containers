2b9ae0bce37f40c562cd3572bb22f646820f3408
Devin Buhl <onedr0p@users.noreply.github.com>
2024-12-03 11:51:21 -0500

    Delete apps/volsync directory (#1143)

--- apps/volsync/ci/latest.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-version="$(curl -sX GET "https://api.github.com/repos/backube/volsync/releases/latest" | jq --raw-output '.tag_name' 2>/dev/null)"
-version="${version#*v}"
-version="${version#*release-}"
-printf "%s" "${version}"
