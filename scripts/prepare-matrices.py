7855966209d957d70dc8f8eaf39da84b02424b3c
Bernd Schorgers <me@bjw-s.dev>
2025-02-18 09:00:24 +0100

    Update workflows

--- scripts/prepare-matrices.py
+++ scripts/prepare-matrices.py
@@ -66,7 +66,7 @@ def get_published_version(image_name):
             # Assume the longest string is the complete version number
             return max(tags, key=len)
 
-def get_image_metadata(subdir, meta, force=False, channels=None):
+def get_image_metadata(subdir, meta, forRelease=False, force=False, channels=None):
     imagesToBuild = {
         "images": [],
         "imagePlatforms": []
@@ -110,7 +110,7 @@ def get_image_metadata(subdir, meta, force=False, channels=None):
         # Platform Metadata
         for platform in channel["platforms"]:
 
-            if platform not in TESTABLE_PLATFORMS:
+            if platform not in TESTABLE_PLATFORMS and not forRelease:
                 continue
 
             toBuild.setdefault("platforms", []).append(platform)
@@ -146,7 +146,8 @@ def get_image_metadata(subdir, meta, force=False, channels=None):
 
 if __name__ == "__main__":
     apps = sys.argv[1]
-    force = sys.argv[2] == "true"
+    forRelease = sys.argv[2] == "true"
+    force = sys.argv[3] == "true"
     imagesToBuild = {
         "images": [],
         "imagePlatforms": []
@@ -169,7 +170,7 @@ if __name__ == "__main__":
             elif os.path.isfile(os.path.join("./apps", app, "metadata.json")):
                 meta = load_metadata_file_json(os.path.join("./apps", app, "metadata.json"))
 
-            imageToBuild = get_image_metadata(os.path.join("./apps", app), meta, force=force, channels=channels)
+            imageToBuild = get_image_metadata(os.path.join("./apps", app), meta, forRelease, force=force, channels=channels)
             if imageToBuild is not None:
                 imagesToBuild["images"].extend(imageToBuild["images"])
                 imagesToBuild["imagePlatforms"].extend(imageToBuild["imagePlatforms"])
@@ -184,7 +185,7 @@ if __name__ == "__main__":
                 else:
                     continue
                 if meta is not None:
-                    imageToBuild = get_image_metadata(subdir, meta, force=force)
+                    imageToBuild = get_image_metadata(subdir, meta, forRelease, force=force)
                     if imageToBuild is not None:
                         imagesToBuild["images"].extend(imageToBuild["images"])
                         imagesToBuild["imagePlatforms"].extend(imageToBuild["imagePlatforms"])
