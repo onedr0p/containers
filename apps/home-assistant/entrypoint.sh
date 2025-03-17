332e04c82571e80eb8472d2912104399ab5d37c0
Devin Buhl <devin@buhl.casa>
2025-03-16 22:02:25 -0400

    feat: container-structure-test
    Signed-off-by: Devin Buhl <devin@buhl.casa>

---
+++ apps/home-assistant/entrypoint.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+#shellcheck disable=SC2086
+
+unset UV_SYSTEM_PYTHON
+mkdir -p "${VENV_FOLDER}"
+uv venv --system-site-packages --link-mode=copy --allow-existing "${VENV_FOLDER}"
+source "${VENV_FOLDER}/bin/activate"
+uv pip freeze --system | grep ^uv= | xargs uv pip install
+
+exec \
+    python3 -m homeassistant \
+        --config /config \
+        "$@"
