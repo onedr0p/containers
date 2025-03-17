b557cbd7e59fe7a67babc43db3ce7c1c2d9ab7f8
Devin Buhl <onedr0p@users.noreply.github.com>
2025-03-17 07:29:51 -0400

    feat: home-assistant log to stdout (#1197)
    Signed-off-by: Devin Buhl <devin@buhl.casa>

--- apps/home-assistant/entrypoint.sh
+++ apps/home-assistant/entrypoint.sh
@@ -7,6 +7,8 @@ uv venv --system-site-packages --link-mode=copy --allow-existing "${VENV_FOLDER}
 source "${VENV_FOLDER}/bin/activate"
 uv pip freeze --system | grep ^uv= | xargs uv pip install
 
+ln -sf /proc/self/fd/1 /config/home-assistant.log
+
 exec \
     python3 -m homeassistant \
         --config /config \
